7eb47129841d216d5ae0fd99c2ad884b96d4f172
Daniel Martínez Algar <daniel.martinez@adoptaunabuelo.com>
2021-02-20 08:36:37 +0100

    Prueba final

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,2 @@
-print("Esto es una preuba de python\n en diferentes líneas")
+print("Esto es una prueba de python\n en diferentes líneas")
+print("Python es fácil y sencillo de aprender\n...\n...\n...\nLo anterior han sido 3 saltos de línea en la misma línea de código")
